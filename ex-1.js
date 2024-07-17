3a8921253bc3d9f277f89e6abfa434c8c33f3393
paphonwat <paphonwat.w@gmail.com>
2024-07-17 14:51:54 +0700

    feat:do ex1-ex2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,19 @@
 function findStudentIndex(students, searchStudent) {
 	// เริ่มเขียนโค้ดตรงนี้จ้า
+	//ถ้า index ไม่เท่ากับ -1 คือเจอนักเรียน แต่ถ้าไม่เจอก็จะ return เป็น -1
+	const index = students.indexOf(searchStudent);
+	if (index !== -1) {
+		return index;
+	} else {
+		return -1;
+	}
 }
 
 // ตอบคำถามตรงนี้จ้า
+let studentsA = ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"];
+let searchStudentA = "John";
+console.log(findStudentIndex(studentsA, searchStudentA));
+
+let studentsB = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"];
+let searchStudentB = "Andrew";
+console.log(findStudentIndex(studentsB, searchStudentB));
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,25 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  const index = books.findIndex(book => book.title === searchTitle);
+    if (index !== -1) {
+        return index;
+    } else {
+        return -1;
+    }
 }
+
+let books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" }
+];
+let searchBookA = "Gone with the Wind";
+console.log(findBookIndex(books, searchBookA));
+
+let searchBookB = "The Master Mind";
+console.log(findBookIndex(books, searchBookB));
